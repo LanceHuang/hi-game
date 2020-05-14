287437d5fb23fc4417cd6208620d9bc06281e554
634202275@qq.com <huanjmaxiada520>
2020-05-14 18:07:32 +0800

    删除旧代码

--- hi-game-demo/src/test/java/com/lance/game/demo/module/item/service/IItemServiceTest.java
+++ hi-game-demo/src/test/java/com/lance/game/demo/module/item/service/IItemServiceTest.java
@@ -1,9 +1,13 @@
 package com.lance.game.demo.module.item.service;
 
 import com.lance.game.demo.module.item.model.AbstractItem;
+import com.lance.game.orm.MongoDaoScanner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,11 +15,19 @@ import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = IItemServiceTest.class)
-@ComponentScan({"com.lance.game.demo.module.item"})
+@Configuration
+@ComponentScan("com.lance.game.demo.module.item")
 public class IItemServiceTest {
 
     @Resource
     private IItemService itemService;
+
+    @Bean
+    public MongoDaoScanner mongoDaoScanner() {
+        MongoDaoScanner mongoDaoScanner = new MongoDaoScanner();
+        mongoDaoScanner.setBasePackage("com.lance.game.demo.module.item.dao");
+        return mongoDaoScanner;
+    }
 
     @Test
     public void test() {
@@ -26,6 +38,4 @@ public class IItemServiceTest {
         System.out.println(item.getType());
         System.out.println(item.getNum());
     }
-
-
 }
