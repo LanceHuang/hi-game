d7bb71b9a6363c44ae0f3395cb67d44f4965a01f
634202275@qq.com <huanjmaxiada520>
2020-05-15 18:00:43 +0800

    AbstractMongoRunner测试代码

--- hi-game-orm/src/test/java/com/lance/game/orm/runner/AbstractMongoRunnerTest.java
+++ hi-game-orm/src/test/java/com/lance/game/orm/runner/AbstractMongoRunnerTest.java
@@ -1,20 +1,39 @@
 package com.lance.game.orm.runner;
 
 import com.lance.game.orm.DefaultMongoDataSource;
+import com.lance.game.orm.MongoDataSource;
 import com.lance.game.orm.handler.DocumentHandler;
 import org.bson.Document;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = AbstractMongoRunnerTest.class)
+@Configuration
 public class AbstractMongoRunnerTest {
 
+    @Resource
     private AbstractMongoRunner mongoRunner;
 
-    @BeforeEach
-    public void beforeEach() {
+    @Bean(initMethod = "init", destroyMethod = "close")
+    public DefaultMongoDataSource mongoDataSource() {
         DefaultMongoDataSource mongoDataSource = new DefaultMongoDataSource();
         mongoDataSource.setUrl("mongodb://localhost:27017");
-        this.mongoRunner = new DefaultMongoRunner(mongoDataSource);
+        mongoDataSource.setMaxActive(3);
+        return mongoDataSource;
+    }
+
+    @Bean
+    public AbstractMongoRunner mongoRunner(MongoDataSource mongoDataSource) {
+        DefaultMongoRunner runner = new DefaultMongoRunner(mongoDataSource);
+        runner.setBatchCount(50);
+        return runner;
     }
 
     @Test
