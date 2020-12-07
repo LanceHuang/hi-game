6cb289e7cefb0f909081746e877f267d7be57b53
634202275@qq.com <huanjmaxiada520>
2020-12-07 12:41:34 +0800

    删除冗余getter/setter方法

--- hi-game-net/src/test/java/com/lance/game/net/TestRequest.java
+++ hi-game-net/src/test/java/com/lance/game/net/TestRequest.java
@@ -1,47 +1,18 @@
 package com.lance.game.net;
 
 import com.lance.game.net.annotation.Protocol;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Lance
  */
+@Getter
+@Setter
 @Protocol(10086)
 public class TestRequest {
 
-    private int    activityId;
+    private int activityId;
     private String account;
-    private long   value;
-
-    public int getActivityId() {
-        return activityId;
-    }
-
-    public void setActivityId(int activityId) {
-        this.activityId = activityId;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public long getValue() {
-        return value;
-    }
-
-    public void setValue(long value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return "TestRequest{" +
-                "activityId=" + activityId +
-                ", account='" + account + '\'' +
-                ", value=" + value +
-                '}';
-    }
+    private long value;
 }
--- hi-game-net/src/test/java/com/lance/game/net/TestResponse.java
+++ hi-game-net/src/test/java/com/lance/game/net/TestResponse.java
@@ -1,29 +1,17 @@
 package com.lance.game.net;
 
 import com.lance.game.net.annotation.Protocol;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Lance
  */
+@Getter
+@Setter
 @Protocol(10087)
 public class TestResponse {
 
     private String account;
-    private int    rank;
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public int getRank() {
-        return rank;
-    }
-
-    public void setRank(int rank) {
-        this.rank = rank;
-    }
+    private int rank;
 }
